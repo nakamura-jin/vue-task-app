3c8a6ab4f5122732a804baaa4f21e3be558009dd
中村　迅 <shu.kyu0224@icloud.com>
2022-06-24 15:55:09 +0900

    header作成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,35 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+// font-awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  faCirclePlus,
+  faXmark,
+  faTrashCan,
+  faCheck,
+  faEllipsis,
+  faUser,
+  faHouse,
+  faRightFromBracket,
+  faChevronDown,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faCirclePlus,
+  faXmark,
+  faTrashCan,
+  faCheck,
+  faEllipsis,
+  faUser,
+  faHouse,
+  faRightFromBracket,
+  faChevronDown
+);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false
 
 new Vue({
