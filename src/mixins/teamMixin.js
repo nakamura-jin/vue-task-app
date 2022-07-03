0ebc2965a6ca588bad32ef31a1deb1a4e8040eae
中村　迅 <shu.kyu0224@icloud.com>
2022-07-03 17:37:46 +0900

    権限なしユーザーログイン時のグループ表示修正

--- src/mixins/teamMixin.js
+++ src/mixins/teamMixin.js
@@ -20,15 +20,17 @@ export default {
       $blockui.show();
       await $http.get('/team').then((response) => {
         const role = JSON.parse(sessionStorage.getItem('data')).role;
+        const login_user = JSON.parse(sessionStorage.getItem('data')).user;
 
         // 権限ありの場合は全て、ない場合ば参加グループのみを表示させる
         if (role === 1)
           this.$store.dispatch('teams/setTeams', response.data.teams);
         else if (role === 2) {
           const teams = [];
+          console.log(response.data.teams);
           response.data.teams.forEach((team) => {
             team.users.forEach((user) => {
-              if (user.id === role) {
+              if (user.id === login_user) {
                 teams.push(team);
               }
             });
